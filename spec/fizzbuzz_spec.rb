b6bb8f5be29638401fba1e313de98d74d49b9fa3
Simone Smith <simonecaplin@gmail.com>
2018-05-10 10:45:07 +0000

    change test and code to return numbers not divisible by 3 or 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
